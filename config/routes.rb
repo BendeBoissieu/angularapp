c689bf00d6668d0e5de3615794c0dffc0918ab72
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-07-18 14:13:48 +0200

    verification authenticity iin appplication_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/create'
+  get 'posts/show'
+  get 'posts/update'
+  get 'posts/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'application#index'
+    resources :post, only: [:index, :show, :create, :update, :destroy]
 end
