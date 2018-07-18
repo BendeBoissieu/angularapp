4e5a6c8cff06e4ca03fab713be91b78389db97ec
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-07-18 14:15:48 +0200

    generate controller Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,49 @@
 class PostsController < ApplicationController
-  def index
-  end
 
-  def create
-  end
+   def index
+     posts = Post.all
+     render json: posts
+   end
 
-  def show
-  end
+   def create
+     post = Post.new(post_params)
+     if post.save
+      render json: post
+     else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+     end
+   end
 
-  def update
-  end
+   def show
+     post = Post.find(params[:id])
+     render json: post
+   end
 
-  def destroy
-  end
+   def update
+     post = Post.find(params[:id])
+     if post.update
+      render json: post
+     else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+     end
+   end
+
+   def update
+     post = Post.find(params[:id])
+     if post.update(post_params)
+      render json: post
+     else
+      render json: post
+     end
+   end
+
+   def destroy
+     post = Post.find(params[:id])
+     render json: post
+   end
+
+   private
+      def post_params
+        params.require(:post).permit(:content)
+      end
 end
